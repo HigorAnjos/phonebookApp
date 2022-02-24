917c8687600c7624436393dd2d81a1b6fdd0ac99
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-02-23 22:04:48 -0300

    login: estrutura basica

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor () {
+    super();
+
+    this.state = {
+      fildEmail: '',
+      fildPassword: '',
+    }
+  }
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return(
-      <h1>Login</h1>
+    const { fildEmail, fildPassword } = this.state;
+    return (
+      <>
+        <h1>Login</h1>
+        <form>
+          <label htmlFor="email">
+            <input
+              id="email"
+              placeholder="email"
+              type="email"
+              name="fildEmail"
+              value={ fildEmail }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="password">
+            <input
+              id="password"
+              placeholder="senha"
+              type="password"
+              name="fildPassword"
+              value={ fildPassword }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button type="submit">Entrar</button>
+        </form>
+      </>
     );
   }
 }
