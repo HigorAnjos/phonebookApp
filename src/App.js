581a010340031637f8cde195bb30a5f1a753ebbe
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-02-23 21:53:53 -0300

    Componentes: login + register

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Login from './components/Login/Login'
+import Register from './components/Register/Register'
 
 function App() {
   return (
     <div className="App">
-      <h1> Agenda telefonica </h1>
+        <h1> Agenda telefonica </h1>
+      <main>
+        <section>
+          <Login />
+        </section>
+        <section>
+          <Register />
+        </section>
+      </main>
     </div>
   );
 }
