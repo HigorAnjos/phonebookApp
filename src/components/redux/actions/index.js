b270bb3752836f5d72437c18246b864ce4dd69da
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-02-27 20:11:58 -0300

    Reducer auth + actions

--- src/components/redux/actions/index.js
+++ src/components/redux/actions/index.js
@@ -1,16 +1,29 @@
 // ACTION TYPES
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
+const SET_LOGIN = 'SET_LOGIN';
+const SET_LOGOUT = 'SET_LOGOUT';
 
+export const actionsTypes = {
+  SET_USER_PROFILE,
+}
 // ACTION CREATORS
 const setUserProfile = (userInfo) => ({
   type: SET_USER_PROFILE,
   payload: userInfo,
 });
 
-export const actionsTypes = {
-  SET_USER_PROFILE,
-}
+const setLogin = () => ({
+  type: SET_LOGIN,
+  payload: true,
+});
+
+const setLogout = () => ({
+  type: SET_LOGOUT,
+  payload: false,
+});
 
 export const actionsCreators = {
   setUserProfile,
+  setLogin,
+  setLogout,
 }
