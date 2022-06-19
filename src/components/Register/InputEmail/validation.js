1b1d985640e13b176ab8b7d5582a64198ef5e608
Higor Anjos <higorc.anjos@gmail.com>
2022-06-19 18:59:02 -0300

    style(validation email): :lipstick: identacao

--- src/components/Register/InputEmail/validation.js
+++ src/components/Register/InputEmail/validation.js
@@ -1,13 +1,15 @@
 import Joi from 'joi';
 
-const shemaEmail = Joi.string().email({ tlds: { allow: false } }).required().messages({
-  'string.email': 'email nao e valido',
-  'string.empty': 'email nao pode ser vazio',
-  'any.required': 'email e obrigatorio',
-});
+const schemaEmail = Joi.string().email({ tlds: { allow: false } })
+  .required()
+  .messages({
+    'string.email': 'email nao e valido',
+    'string.empty': 'email nao pode ser vazio',
+    'any.required': 'email e obrigatorio',
+  });
 
 const hasEmailError = (email) => {
-  const { error } = shemaEmail.validate(email);
+  const { error } = schemaEmail.validate(email);
 
   if (error) {
     return error.details[0].message; // retorna o erro
